403b94db82c6ea8a044854e94ab3dcdac6f61e48
VishankSingh <68466318+VishankSingh@users.noreply.github.com>
2020-11-07 16:34:54 +0530

    Add files via upload

---
+++ mandelbrot_color.py
@@ -0,0 +1,40 @@
+from PIL import Image, ImageDraw #run "pip install pillow in cmd"
+
+MAX_ITER = 80 #increase this to increase the detail 
+
+def mandelbrot(c):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < MAX_ITER:
+        z = z*z + c
+        n += 1
+    return n
+
+# Image size (pixels)
+WIDTH = 600
+HEIGHT = 400
+
+# Plot window
+RE_START = -2
+RE_END = 1
+IM_START = -1
+IM_END = 1
+
+im = Image.new('HSV', (WIDTH, HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        # Convert pixel coordinate to complex number
+        c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
+        # Compute the number of iterations
+        m = mandelbrot(c)
+        # The color depends on the number of iterations
+        hue = int(255 * m / MAX_ITER)
+        saturation = 255
+        value = 255 if m < MAX_ITER else 0
+        # Plot the point
+        draw.point([x, y], (hue, saturation, value))
+
+im.convert('RGB').save('rgb_output.png', 'PNG')
